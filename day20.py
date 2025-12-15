09d1eacaece6eb0066a7293d17fa66fa7130dba0
pahasara-ridmaka <pridmaka@gmail.com>
2025-12-15 11:28:34 +0530

    Day20 Completed

---
+++ day20.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    numberOfSwaps = 0
+    for i in range(n):
+        for j in range(n - i - 1):
+            if a[j] > a[j+1]:
+                a[j], a[j+1] = a[j+1], a[j]
+                numberOfSwaps += 1
+
+        if numberOfSwaps == 0:
+            break
+
+    print(f"Array is sorted in {numberOfSwaps} swaps.")
+    print(f"First Element: {a[0]}")
+    print(f"Last Element: {a[-1]}")
+
